22747d63492f48f671076a4f997cb5a17dfb66d5
Ke Yang <key@goretailpro.com>
2015-10-28 15:52:38 -0500

    first

---
+++ app/code/local/Ke/CustomAttribute/Model/Observer.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Ke
+ * @package     Ke_CustomAttribute
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+class Ke_Customattribute_Model_Observer extends Varien_Object
+{
+    public function salesQuoteItemSetCustomAttribute($observer)
+    {
+        $quoteItem = $observer->getQuoteItem();
+        $product = $observer->getProduct();
+        $quoteItem->setCustomAttribute($product->getCustomAttribute());
+    }
+    public function setColorAttribute(Varien_Event_Observer $observer) {
+
+    $item = $observer->getQuoteItem();
+    $product = $observer->getProduct();
+    $item->setColor($product->getColor());
+    return $this;
+	}
+	public function setSizeAttribute(Varien_Event_Observer $observer) {
+
+    $item = $observer->getQuoteItem();
+    $product = $observer->getProduct();
+    $item->setSize($product->getSize());
+    return $this;
+	}
+    public function setWebSkuAttribute(Varien_Event_Observer $observer) {
+
+    $item = $observer->getQuoteItem();
+    $product = $observer->getProduct();
+    $item->setSize($product->getWebSku());
+    return $this;
+    }
+}
